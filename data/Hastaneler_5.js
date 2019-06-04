aaf39ecc2422649959543799a023d810551db84b
bayindiribrahim <37131318+bayindiribrahim@users.noreply.github.com>
2019-06-04 04:15:42 +0300

    Add files via upload

---
+++ data/Hastaneler_5.js
@@ -0,0 +1,13 @@
+var json_Hastaneler_5 = {
+"type": "FeatureCollection",
+"name": "Hastaneler_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ADI": "S.S.K. Ulus Hastanesi", "MAHALLE": "Fevzi Paşa" }, "geometry": { "type": "Point", "coordinates": [ 32.849453447588928, 39.941944110644876 ] } },
+{ "type": "Feature", "properties": { "ADI": "Hacettepe Üniversitesi Hastanesi", "MAHALLE": "Demirtaş" }, "geometry": { "type": "Point", "coordinates": [ 32.860869245786098, 39.929583799093528 ] } },
+{ "type": "Feature", "properties": { "ADI": "Ankara Yüksek Ihtisas Hastanesi", "MAHALLE": "Ülkü" }, "geometry": { "type": "Point", "coordinates": [ 32.857251820316783, 39.929971173780039 ] } },
+{ "type": "Feature", "properties": { "ADI": "Belediye Hastanesi", "MAHALLE": "Korkut Reis" }, "geometry": { "type": "Point", "coordinates": [ 32.852547662747476, 39.926700521125738 ] } },
+{ "type": "Feature", "properties": { "ADI": "Ibni Sina Hastanesi", "MAHALLE": "Ülkü" }, "geometry": { "type": "Point", "coordinates": [ 32.858588160164103, 39.932913350596877 ] } },
+{ "type": "Feature", "properties": { "ADI": "Numune Hastanesi", "MAHALLE": "Ülkü" }, "geometry": { "type": "Point", "coordinates": [ 32.857181132784689, 39.932672960537388 ] } }
+]
+}
